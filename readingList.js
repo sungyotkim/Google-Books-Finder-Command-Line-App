a9546d9c3907edec14693b87011d4a1f052f73f9
sungyotkim <sungyotkim@gmail.com>
2022-12-19 18:07:16 -0500

    add error handling for no books added

--- readingList.js
+++ readingList.js
@@ -35,7 +35,9 @@ export class ReadingList {
       this.readingList.push(book);
     });
 
-    if (books.length === 1) {
+    if (books.length === 0) {
+      console.log("No books were added to your list.");
+    } else if (books.length === 1) {
       console.log(`${titles} was added to your list.`);
     } else {
       console.log(`${titles} were added to your list.`);
